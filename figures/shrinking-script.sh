78c15bd3408ecc5cfddde6f76e40258fad8dac8f
Michael Eastwood <mweastwood@astro.caltech.edu>
2017-06-27 10:55:27 -0700

    reorganize the figures directory

---
+++ figures/shrinking-script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+convert spw04.png -resize 256x256 -set colorspace RGB spw04-small.png
+convert spw06.png -resize 256x256 -set colorspace RGB spw06-small.png
+convert spw08.png -resize 256x256 -set colorspace RGB spw08-small.png
+convert spw10.png -resize 256x256 -set colorspace RGB spw10-small.png
+convert spw12.png -resize 256x256 -set colorspace RGB spw12-small.png
+convert spw14.png -resize 256x256 -set colorspace RGB spw14-small.png
+convert spw16.png -resize 256x256 -set colorspace RGB spw16-small.png
+convert spw18.png -resize 256x256 -set colorspace RGB spw18-small.png
