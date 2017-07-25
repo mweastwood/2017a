097ce568a6f74bd02fbb4aea6ea3327e1148a42c
Michael Eastwood <mweastwood@astro.caltech.edu>
2017-07-25 16:26:26 -0700

    update the lcurve plot
    and stop tracking the jld file

--- figures/lcurve/make.jl
+++ figures/lcurve/make.jl
@@ -1,20 +1,32 @@
 using JLD, PyPlot
 
 path = dirname(@__FILE__)
-#trials, regnorm, lsnorm = load(joinpath(path, "lcurve.jld"), "trials", "regnorm", "lsnorm")
-trials, regnorm, lsnorm = load(joinpath(path, "18.jld"), "trials", "regnorm", "lsnorm")
+trials, regnorm, lsnorm = load(joinpath(path, "lcurve.jld"), "trials", "regnorm", "lsnorm")
+regnorm /= 10000
+lsnorm  /= 10000
+
+function do_annotation(idx, x, y)
+    annotate(@sprintf("ε=%g", trials[idx]), xy=(regnorm[idx], lsnorm[idx]),
+             xytext=(regnorm[idx]+x, lsnorm[idx]+y),
+             arrowprops=Dict(:width=>1, :headwidth=>5, :headlength=>5,
+                             :shrink=>0.05, :facecolor=>"black"),
+             horizontalalignment=x≤0 ? "center" : "left",
+             fontsize=16)
+end
+
 
 close("all")
-figure(1, figsize=(6, 4))
+figure(1)
+
+gca()[:tick_params](axis="both", which="major", labelsize=16)
 plot(regnorm, lsnorm, "k-")
-for idx = 8:7:36
-    annotate(@sprintf("ε=%g", trials[idx]), xy=(regnorm[idx], lsnorm[idx]),
-             xytext=(regnorm[idx]+60000, lsnorm[idx]+20000),
-             arrowprops=Dict(:width=>1, :headwidth=>5, :headlength=>5,
-                             :shrink=>0.05, :facecolor=>"black"))
-end
-xlabel(L"\Vert a \Vert", fontsize=12)
-ylabel(L"\Vert v - Ba \Vert", fontsize=12)
+do_annotation(8, 10, 10)
+do_annotation(15, 10, 10)
+do_annotation(22, 10, 10)
+do_annotation(29, 0, 10)
+do_annotation(36, 10, 10)
+xlabel(L"\Vert a \Vert" * " (arbitrary units)", fontsize=16)
+ylabel(L"\Vert v - Ba \Vert" * " (arbitrary units)", fontsize=16)
 tight_layout()
 
 savefig(joinpath(path, "lcurve.pdf"), bbox_inches="tight", pad_inches=0.1, transparent=true)
