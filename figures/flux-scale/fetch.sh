0f769759b0d8549f1deee96a1b344a237244dcfc
Michael Eastwood <mweastwood@astro.caltech.edu>
2017-09-04 10:50:06 -0700

    add error bars to the flux scale plot

--- figures/flux-scale/fetch.sh
+++ figures/flux-scale/fetch.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-scp mweastwood@astm01.lwa.ovro.caltech.edu:/lustre/mweastwood/mmode-analysis/workspace/tmp/source-fluxes-map-wiener-filtered.jld .
-scp mweastwood@astm01.lwa.ovro.caltech.edu:/lustre/mweastwood/mmode-analysis/workspace/tmp/source-fluxes-map-cyga-peeled.jld .
-scp mweastwood@astm01.lwa.ovro.caltech.edu:/lustre/mweastwood/mmode-analysis/workspace/tmp/source-fluxes-map-vira-peeled.jld .
-scp mweastwood@astm01.lwa.ovro.caltech.edu:/lustre/mweastwood/mmode-analysis/workspace/tmp/source-fluxes-map-hyaa-peeled.jld .
-scp mweastwood@astm01.lwa.ovro.caltech.edu:/lustre/mweastwood/mmode-analysis/workspace/tmp/source-fluxes-map-perb-peeled.jld .
-scp mweastwood@astm01.lwa.ovro.caltech.edu:/lustre/mweastwood/mmode-analysis/workspace/tmp/source-fluxes-map-3c353-peeled.jld .
+scp mweastwood@astm01.lwa.ovro.caltech.edu:/lustre/mweastwood/mmode-analysis/workspace/spectra.jld .
 
--- figures/flux-scale/make.jl
+++ figures/flux-scale/make.jl
@@ -4,66 +4,64 @@ using JLD, PyPlot
 using CasaCore.Measures
 
 path = dirname(@__FILE__)
-close("all")
 
 frequencies = [3.6528e7, 4.176e7, 4.6992e7, 5.2224e7, 5.7456e7, 6.2688e7, 6.792e7, 7.3152e7]
-exceptions = ("Cyg A", "Vir A", "Hya A", "Per B", "3C 353")
-cutoff = frequencies .> 50e6 # Perley fluxes only valid above 50 MHz
+model_frequencies = linspace(10e6, 100e6, 201)
 
-calibrators, measured_fluxes, perley_fluxes, scaife_fluxes, baars_fluxes =
-    load(joinpath(path, "source-fluxes-map-wiener-filtered.jld"),
-         "calibrators", "measured_fluxes", "perley_fluxes", "scaife_fluxes", "baars_fluxes")
+everything, odd, even, baars, perley, scaife =
+    load(joinpath(path, "spectra.jld"),
+         "everything", "odd", "even", "baars", "perley", "scaife")
+
 
 # manually spell this out so that we can pick the order of the plots
-#names = collect(keys(calibrators))
 names = ("Cyg A", "Hya A", "Lyn A", "Per B", "Vir A", "3C 48", "3C 147", "3C 286", "3C 295",
          "3C 353", "3C 380")
 
-figure(1, figsize=(10, 10)); clf()
+figure(1, figsize=(12, 10)); clf()
 for (idx, name) in enumerate(names)
-    direction = calibrators[name]
-    lower = lowercase(replace(name, " ", ""))
-    if name in exceptions
-        temp = load(joinpath(path, "source-fluxes-map-$lower-peeled.jld"), "measured_fluxes")
-        measured = temp[name]
-    else
-        measured = measured_fluxes[name]
-    end
+    μ = everything[name]
+    σ = sqrt.(0.5*(abs2.(odd[name]-μ) + abs2.(even[name]-μ)))
+    max_value = maximum(μ)
 
     subplot(4, 3, idx)
-    gca()[:tick_params](axis="both", which="major", labelsize=14)
+    gca()[:tick_params](axis="both", which="major", labelsize=12)
+    errorbar(frequencies/1e6, μ, yerr=σ, fmt="k.", zorder=3)
 
-    max_value = maximum(measured)
-    plot(frequencies/1e6, measured, "ko", zorder=3)
-    if name in keys(perley_fluxes)
-        model = perley_fluxes[name]
-        plot(frequencies[cutoff]/1e6, model[cutoff], "k-", zorder=3)
-        plot(frequencies/1e6, model, "k:", zorder=2)
-        max_value = max(max_value, maximum(model))
+    if name in keys(perley)
+        model = perley[name]
+        cutoff = 50e6 .< model_frequencies .< 75e6
+        plot(model_frequencies[cutoff]/1e6, model[cutoff], "k-", zorder=3)
+        cutoff = 35e6 .< model_frequencies .< 75e6
+        plot(model_frequencies[cutoff]/1e6, model[cutoff], "k:", zorder=2)
+        #max_value = max(max_value, maximum(model))
     end
-    if name in keys(scaife_fluxes)
-        model = scaife_fluxes[name]
-        plot(frequencies/1e6, model, "k--", zorder=3)
-        max_value = max(max_value, maximum(model))
+    if name in keys(scaife)
+        model = scaife[name]
+        cutoff = 35e6 .< model_frequencies .< 75e6
+        plot(model_frequencies[cutoff]/1e6, model[cutoff], "k--", zorder=3)
+        #max_value = max(max_value, maximum(model))
     end
-    if name in keys(baars_fluxes)
-        model = baars_fluxes[name]
-        plot(frequencies/1e6, model, "k-.", zorder=3)
-        max_value = max(max_value, maximum(model))
+    if name in keys(baars)
+        model = baars[name]
+        cutoff = 35e6 .< model_frequencies .< 75e6
+        plot(model_frequencies[cutoff]/1e6, model[cutoff], "k-.", zorder=3)
+        #max_value = max(max_value, maximum(model))
     end
-    text(0.95, 0.95, name, transform=gca()[:transAxes], fontsize=16,
+
+    text(0.95, 0.95, name, transform=gca()[:transAxes], fontsize=14,
          horizontalalignment="right", verticalalignment="top",
          backgroundcolor="white", zorder=2)
     xlim(35, 75)
-    ylim(0, max(100, 1.2*max_value))
+    ylim(0, max(100.0, gca()[:get_ylim]()[2]))
     idx < 9 && plt[:setp](gca()[:get_xticklabels](), visible=false)
-    idx ≥ 9 && xlabel("Frequency (MHz)", fontsize=14)
-    mod(idx, 3) == 1 && ylabel("Flux (Jy)", fontsize=14)
-    mod(idx, 3) == 1 && gca()[:get_yaxis]()[:set_label_coords](-0.30, 0.5)
+    idx ≥ 9 && xlabel("Frequency (MHz)", fontsize=12)
+    mod(idx, 3) == 1 && ylabel("Flux (Jy)", fontsize=12)
+    mod(idx, 3) == 1 && gca()[:get_yaxis]()[:set_label_coords](-0.20, 0.5)
     grid("on")
 end
 tight_layout()
-gcf()[:subplots_adjust](hspace=0.10, wspace=0.25)
+gcf()[:subplots_adjust](hspace=0.10, wspace=0.20)
+
 savefig(joinpath(path, "flux-scale.pdf"),
         bbox_inches="tight", pad_inches=0.1, transparent=true)
 
