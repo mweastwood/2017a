e973ba3c64591c73aa9238b1db0e04a2b43bd680
Michael Eastwood <mweastwood@astro.caltech.edu>
2017-09-04 10:25:00 -0700

    put source tracks on top of the beam model

--- figures/beam/fetch.sh
+++ figures/beam/fetch.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 scp mweastwood@astm01.lwa.ovro.caltech.edu:/lustre/mweastwood/mmode-analysis/workspace/spw04/beam.jld spw04-beam.jld
+scp mweastwood@astm01.lwa.ovro.caltech.edu:/lustre/mweastwood/mmode-analysis/workspace/spw10/beam.jld spw10-beam.jld
 scp mweastwood@astm01.lwa.ovro.caltech.edu:/lustre/mweastwood/mmode-analysis/workspace/spw18/beam.jld spw18-beam.jld
 
--- figures/beam/make.jl
+++ figures/beam/make.jl
@@ -1,11 +1,12 @@
 #!/usr/bin/env julia
 
-using JLD, PyPlot, CasaCore.Measures
+using JLD, PyPlot, PyCall, CasaCore.Measures
+
+@pyimport mpl_toolkits.axes_grid1 as tk
+@pyimport matplotlib.patheffects as pe
 
 path = dirname(@__FILE__)
-figure(1, figsize=(15, 5)); clf()
-
-# Create the source track panel
+figure(1, figsize=(12, 4)); clf()
 
 sources = Dict("Cyg A" => Direction(dir"J2000", "19h59m28.35663s", "+40d44m02.0970s"),
                "Cas A" => Direction(dir"J2000", "23h23m24.000s", "+58d48m54.00s"),
@@ -17,18 +18,17 @@ sources = Dict("Cyg A" => Direction(dir"J2000", "19h59m28.35663s", "+40d44m02.09
                "3C 353" => Direction(dir"J2000", "17h20m28.147s", "-00d58m47.12s"))
 
 ovro_lwa = Position(pos"ITRF", -2.40927462614919e6, -4.477838733582964e6, 3.839370728766106e6)
-
 times = linspace(4.994049600941745e9, 4.994150416022397e9, 7756)
 
-subplot(1, 3, 1)
+tracks = Dict{String, Tuple{Vector{Float64}, Vector{Float64}}}()
 frame = ReferenceFrame()
-set!(frame, ovro_lwa)
+Measures.set!(frame, ovro_lwa)
 for name in keys(sources)
     direction = sources[name]
     l = Float64[]
     m = Float64[]
     for t in times
-        set!(frame, Epoch(epoch"UTC", t*seconds))
+        Measures.set!(frame, Epoch(epoch"UTC", t*seconds))
         azel = measure(frame, direction, dir"AZEL")
         az = longitude(azel)
         el =  latitude(azel)
@@ -40,64 +40,77 @@ for name in keys(sources)
             push!(m, NaN)
         end
     end
-    mmin = minimum(m[.!isnan.(m)])
-    if name == "3C 353"
-        annotate(name, xy=(0, mmin+0.01), horizontalalignment="center",
-                 fontsize=12)
-    else
-        annotate(name, xy=(0, mmin+0.02), horizontalalignment="center",
-                 fontsize=12)
-    end
-    plot(l, m, "k-")
+    tracks[name] = (l, m)
 end
-θ = linspace(0, 2π, 1000)
-annotate("Horizon", xy=(0, -0.98), horizontalalignment="center",
-         fontsize=12)
-gca()[:tick_params](axis="both", which="major", labelsize=15)
-plot(cos.(θ), sin.(θ), "k--")
-gca()[:set_aspect]("equal")
-xlim(1, -1)
-ylim(-1, 1)
-xlabel("l (direction cosine)", fontsize=15)
-ylabel("m (direction cosine)", fontsize=15)
 
-# Create the beam panels
+frequencies = [3.6528e7, 5.2224e7, 7.3152e7]
+filenames = ["spw04-beam", "spw10-beam", "spw18-beam"]
 
-frequencies = [3.6528e7, 7.3152e7]
-filenames = ["spw04-beam", "spw18-beam"]
-
-global _im
-for idx = 1:length(filenames)
-    subplot(1, 3, idx+1)
-    filename = filenames[idx]
-
+function plot_image(filename)
     img = load(joinpath(path, filename*".jld"), "I-image")
-    
-    gca()[:tick_params](axis="both", which="major", labelsize=15)
-    
+    gca()[:tick_params](axis="both", which="major", labelsize=12)
+    gca()[:set_aspect]("equal")
     circle = plt[:Circle]((0, 0), 1, alpha=0)
     gca()[:add_patch](circle)
-    _im = imshow(img, extent=(-1, 1, -1, 1), interpolation="nearest", vmin=0, vmax=1,
+    out = imshow(img, extent=(-1, 1, -1, 1), interpolation="nearest", vmin=0, vmax=1,
                  cmap=get_cmap("magma"), clip_path=circle)
-    gca()[:set_aspect]("equal")
     xlim(1, -1)
     ylim(-1, 1)
-    plt[:setp](gca()[:get_yticklabels](), visible=false)
-    xlabel("l (direction cosine)", fontsize=15)
-    txt = text(0.95, 0.95, @sprintf("%.3f MHz", frequencies[idx]/1e6),
-               transform=gca()[:transAxes], fontsize=18, fontweight="bold",
-               horizontalalignment="right", verticalalignment="top",
-               color="black", zorder=2)
-    txt[:set_bbox](Dict(:facecolor=>"white", :alpha=>0.5, :edgecolor=>"none"))
+    out
 end
 
-gcf()[:subplots_adjust](wspace=0.10)
+function plot_tracks()
+    for (l, m) in values(tracks)
+        plot(l, m, "w-", alpha=0.5)
+    end
+end
 
-cax = gcf()[:add_axes]([0.92, 0.13, 0.023, 0.73])
+function label(idx)
+    txt = text(0.95, 0.95, @sprintf("%.3f MHz", frequencies[idx]/1e6),
+               transform=gca()[:transAxes], fontsize=16, fontweight="bold",
+               horizontalalignment="right", verticalalignment="top",
+               color="white", zorder=2)
+    #txt[:set_bbox](Dict(:facecolor=>"white", :alpha=>0.75, :edgecolor=>"none"))
+    txt[:set_path_effects]([pe.Stroke(linewidth=2, foreground="black"),
+                            pe.Normal()])
+end
+
+ax = gca()
+divider = tk.make_axes_locatable(ax)
+
+_im = plot_image(filenames[1])
+plot_tracks()
+label(1)
+xlabel("l (direction cosine)", fontsize=12)
+ylabel("m (direction cosine)", fontsize=12)
+xticks([1.0, 0.5, 0.0, -0.5])
+yticks([1.0, 0.5, 0.0, -0.5, -1.0])
+
+_ax = divider[:append_axes]("right", size="100%", pad=0.10)
+plot_image(filenames[2])
+plot_tracks()
+label(2)
+xlabel("l (direction cosine)", fontsize=12)
+plt[:setp](gca()[:get_yticklabels](), visible=false)
+xticks([1.0, 0.5, 0.0, -0.5])
+yticks([1.0, 0.5, 0.0, -0.5, -1.0])
+
+_ax = divider[:append_axes]("right", size="100%", pad=0.10)
+plot_image(filenames[3])
+plot_tracks()
+label(3)
+xlabel("l (direction cosine)", fontsize=12)
+plt[:setp](gca()[:get_yticklabels](), visible=false)
+xticks([1.0, 0.5, 0.0, -0.5, -1.0])
+yticks([1.0, 0.5, 0.0, -0.5, -1.0])
+
+cax = divider[:append_axes]("right", size="5%", pad=0.10)
 cbar = colorbar(_im, cax=cax)
-cbar[:ax][:tick_params](labelsize=15)
-cbar[:set_label]("normalized amplitude", fontsize=15, rotation=270)
-cbar[:ax][:get_yaxis]()[:set_label_coords](3.3, 0.5)
+cbar[:ax][:tick_params](labelsize=12)
+cbar[:set_label]("normalized amplitude", fontsize=12, rotation=270)
+cbar[:ax][:get_yaxis]()[:set_label_coords](4.5, 0.5)
+
+tight_layout()
 
 savefig(joinpath(path, "beam.pdf"),
         bbox_inches="tight", pad_inches=0, transparent=true)
