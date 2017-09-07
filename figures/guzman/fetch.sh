09672af5d27b44317cf3866c81aa6881cc1628a4
Michael Eastwood <mweastwood@astro.caltech.edu>
2017-09-07 10:59:23 -0700

    update the comparison with the guzman maps

---
+++ figures/guzman/fetch.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+scp mweastwood@astm01.lwa.ovro.caltech.edu:/lustre/mweastwood/mmode-analysis/pipeline/Comparisons/comparison-with-guzman.jld .
+
--- figures/guzman/make.jl
+++ figures/guzman/make.jl
@@ -1,25 +1,38 @@
-using JLD, PyPlot
+#!/usr/bin/env julia
+
+using JLD, PyPlot, LibHealpix
 
 path = dirname(@__FILE__)
-close("all")
 
-guzman, lwa = load(joinpath(path, "comparison-with-guzman.jld"), "guzman", "lwa")
+δ = load(joinpath(path, "comparison-with-guzman.jld"), "difference")
+img = mollweide(RingHealpixMap(δ))
 
-figure(1); clf()
+figure(1, figsize=(12, 5)); clf()
+
 θ = linspace(0, 2π, 512)
 x = 2cos.(θ)
 y = sin.(θ)
+
 ellipse = plt[:Polygon]([x y], alpha=0)
 gca()[:add_patch](ellipse)
-imshow((lwa-guzman)./guzman, interpolation="nearest", cmap=get_cmap("RdBu_r"),
+imshow(img, interpolation="nearest", cmap=get_cmap("RdBu_r"),
        vmin=-0.5, vmax=+0.5,
-       extent=(-2, 2, -1, 1), clip_path=ellipse)
+       extent=(-2, 2, -1, 1),
+       clip_path=ellipse, zorder=10)
+xlim(-2, 2)
+ylim(-1, 1)
 gca()[:set_aspect]("equal")
+gca()[:get_xaxis]()[:set_visible](false)
+gca()[:get_yaxis]()[:set_visible](false)
 axis("off")
-cbar = colorbar(fraction=0.02)
-cbar[:ax][:tick_params](labelsize=16)
+
+cbar = colorbar()
+cbar[:ax][:tick_params](labelsize=12)
+cbar[:set_label]("fractional difference", fontsize=12, rotation=270)
+cbar[:ax][:get_yaxis]()[:set_label_coords](4.0, 0.5)
+
 tight_layout()
 
-savefig(joinpath(path, "guzman-comparison.pdf"),
+savefig(joinpath(path, "guzman.pdf"),
         bbox_inches="tight", pad_inches=0, transparent=true)
 
