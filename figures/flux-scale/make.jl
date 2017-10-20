98a55a95546e4253531993455bb977b4ff9ae705
Michael Eastwood <mweastwood@astro.caltech.edu>
2017-10-20 15:12:20 -0700

    add coauthors and begin to address comments

--- figures/flux-scale/make.jl
+++ figures/flux-scale/make.jl
@@ -1,7 +1,6 @@
 #!/usr/bin/env julia
 
 using JLD, PyPlot
-using CasaCore.Measures
 
 path = dirname(@__FILE__)
 
@@ -14,8 +13,8 @@ everything, odd, even, baars, perley, scaife =
 
 
 # manually spell this out so that we can pick the order of the plots
-names = ("Cyg A", "Hya A", "Lyn A", "Per B", "Vir A", "3C 48", "3C 147", "3C 286", "3C 295",
-         "3C 353", "3C 380")
+names = ("Cas A", "Cyg A", "Hya A", "Lyn A", "Vir A", "3C 48", "Per B", "3C 147", "3C 286",
+         "3C 295", "3C 353", "3C 380")
 
 figure(1, figsize=(12, 10)); clf()
 for (idx, name) in enumerate(names)
@@ -47,14 +46,35 @@ for (idx, name) in enumerate(names)
         plot(model_frequencies[cutoff]/1e6, model[cutoff], "k-.", zorder=3)
         #max_value = max(max_value, maximum(model))
     end
+    if name == "Cas A"
+        cutoff = 35e6 .< model_frequencies .< 75e6
+        model = 10.^(5.625 .- 0.634.*log10.(model_frequencies/1e6)
+                           .- 0.023.*log10.(model_frequencies/1e6).^2)
+        plot(model_frequencies[cutoff]/1e6, model[cutoff], "k-.", zorder=3)
+        text(43, 32000, "1965", rotation=-19, fontsize=12,
+             horizontalalignment="left", verticalalignment="bottom",
+             zorder=2)
+        τ = -1/log10(1-0.0077) # 0.77%/year secular decrease
+        model .*= 10^(-(2017-1965)/τ)
+        plot(model_frequencies[cutoff]/1e6, model[cutoff], "k-.", zorder=3)
+        text(43, 21500, "2017", rotation=-15, fontsize=12,
+             horizontalalignment="left", verticalalignment="top",
+             zorder=2)
+    end
+
+    # Per B is a confusing name (apparently the source is actually in Taurus), so we will relabel
+    # this source as 3C 123.
+    if name == "Per B"
+        name = "3C 123"
+    end
 
     text(0.95, 0.95, name, transform=gca()[:transAxes], fontsize=14,
          horizontalalignment="right", verticalalignment="top",
          backgroundcolor="white", zorder=2)
     xlim(35, 75)
     ylim(0, max(100.0, gca()[:get_ylim]()[2]))
-    idx < 9 && plt[:setp](gca()[:get_xticklabels](), visible=false)
-    idx ≥ 9 && xlabel("Frequency (MHz)", fontsize=12)
+    idx ≤ 9 && plt[:setp](gca()[:get_xticklabels](), visible=false)
+    idx > 9 && xlabel("Frequency (MHz)", fontsize=12)
     mod(idx, 3) == 1 && ylabel("Flux (Jy)", fontsize=12)
     mod(idx, 3) == 1 && gca()[:get_yaxis]()[:set_label_coords](-0.20, 0.5)
     grid("on")
---
+++ figures/three-color/make.jl
@@ -0,0 +1,124 @@
+#!/usr/bin/env julia
+
+using JLD, LibHealpix, PyPlot, PyCall
+
+@pyimport mpl_toolkits.axes_grid1 as tk
+
+path = dirname(@__FILE__)
+
+function allsky()
+    ν1 = 36.528
+    ν2 = 52.224
+    ν3 = 73.152
+    brightness1 = load(joinpath(path, "spw04.jld"), "image")
+    brightness2 = load(joinpath(path, "spw10.jld"), "image")
+    brightness3 = load(joinpath(path, "spw18.jld"), "image")
+    
+    img = zeros(size(brightness1)..., 3)
+    for y = 1:size(img, 2), x = 1:size(img, 1)
+        img[x, y, 1] = brightness1[x, y] * (ν1/ν3)^2.5
+        img[x, y, 2] = brightness2[x, y] * (ν2/ν3)^2.5
+        img[x, y, 3] = brightness3[x, y]
+    end
+
+    figure(1, figsize=(12, 5)); clf()
+
+    θ = linspace(0, 2π, 512)
+    x = 2cos.(θ)
+    y = sin.(θ)
+    ellipse = plt[:Polygon]([x y], alpha=0)
+    gca()[:add_patch](ellipse)
+
+    function decide_on_color_scale(img)
+        pixels = vec(img)
+        pixels = pixels[pixels .!= 0]
+        sort!(pixels)
+        N = length(pixels)
+        min = pixels[round(Int, 0.00001N+1)]
+        max = pixels[round(Int, 0.999N+1)]
+        min, max, (max-min)/10
+    end
+
+    img_min, img_max, img_base = decide_on_color_scale(img)
+    img = clamp.(img, img_min, img_max)
+    img = (log10.(img - img_min + img_base) - log10.(img_base)) ./
+            (log10.(img_max - img_min + img_base) - log10.(img_base))
+    imshow(img, interpolation="nearest",
+           extent=(-2, 2, -1, 1),
+           clip_path=ellipse)
+
+    xlim(-2, 2)
+    ylim(-1, 1)
+    gca()[:set_aspect]("equal")
+    gca()[:get_xaxis]()[:set_visible](false)
+    gca()[:get_yaxis]()[:set_visible](false)
+    axis("off")
+    tight_layout()
+
+    savefig(joinpath(path, "ovro-lwa-sky-map.pdf"),
+            bbox_inches="tight", pad_inches=0, transparent=true)
+end
+
+function cutout(map)
+    lrange = linspace(0, 220, 2048)
+    brange = linspace( -30,  30, 1024)
+    img = zeros(length(brange), length(lrange))
+    for (jdx, l) in enumerate(lrange), (idx, b) in enumerate(brange)
+        img[idx, jdx] = LibHealpix.interpolate(map, deg2rad(90-b), deg2rad(l))
+    end
+    img, lrange, brange
+end
+
+function galaxy()
+    ν1 = 36.528
+    ν2 = 52.224
+    ν3 = 73.152
+    map1 = RingHealpixMap(load(joinpath(path, "spw04.jld"), "map"))
+    map2 = RingHealpixMap(load(joinpath(path, "spw10.jld"), "map"))
+    map3 = RingHealpixMap(load(joinpath(path, "spw18.jld"), "map"))
+    img1, lrange, brange = cutout(map1)
+    img2, lrange, brange = cutout(map2)
+    img3, lrange, brange = cutout(map3)
+
+    img = zeros(size(img1)..., 3)
+    for y = 1:size(img, 2), x = 1:size(img, 1)
+        img[x, y, 1] = img1[x, y] * (ν1/ν3)^2.5
+        img[x, y, 2] = img2[x, y] * (ν2/ν3)^2.5
+        img[x, y, 3] = img3[x, y]
+    end
+
+    function decide_on_color_scale(img)
+        pixels = vec(img)
+        pixels = pixels[pixels .!= 0]
+        sort!(pixels)
+        N = length(pixels)
+        min = pixels[round(Int, 0.00001N+1)]
+        max = pixels[round(Int, 0.999N+1)]
+        min, max, (max-min)/10
+    end
+
+    img_min, img_max, img_base = decide_on_color_scale(img)
+    img = clamp.(img, img_min, img_max)
+    img = (log10.(img - img_min + img_base) - log10.(img_base)) ./
+            (log10.(img_max - img_min + img_base) - log10.(img_base))
+
+    figure(2, figsize=(12, 5)); clf()
+    imshow(img, interpolation="nearest",
+           origin="lower",
+           extent=(minimum(lrange), maximum(lrange), minimum(brange), maximum(brange)))
+    xlabel("l (degrees)", fontsize=12)
+    ylabel("b (degrees)", fontsize=12)
+    
+    xlim(minimum(lrange), maximum(lrange))
+    ylim(minimum(brange), maximum(brange))
+    gca()[:set_aspect]("equal")
+    gca()[:invert_xaxis]()
+    tight_layout()
+
+    savefig(joinpath(path, "ovro-lwa-galactic-plane.pdf"),
+            bbox_inches="tight", pad_inches=0, transparent=true)
+end
+
+allsky()
+galaxy()
+
